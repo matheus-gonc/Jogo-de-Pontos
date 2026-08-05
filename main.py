235d9ed14db2f3a6492843b2e68022517f95a344
Matheus Gonçalves de Souza <matheusgsouza35@gmail.com>
2026-08-05 16:52:47 -0300

    acumulação
    implementação de acumuladores que contam quantas vezes um número aparece na matriz

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ linhas = 5
 colunas = 6
 matriz = []
 
-lista_de_numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 dicionario_de_numeros = {
     '1': 0,
     '2': 0,
@@ -16,6 +15,7 @@ dicionario_de_numeros = {
     '8': 0,
     '9': 0,
 }
+lista_de_numeros = list(dicionario_de_numeros.keys())
 
 # gerar a matriz e colocar números aleatórios nela
 for i in range(linhas):
@@ -25,8 +25,17 @@ for i in range(linhas):
         linha.append(numero)
     matriz.append(linha)
 
+for i in range(linhas):
+    for j in range(colunas):
+        dicionario_de_numeros[matriz[i][j]] += 1
+
+
+
+
+
 
 
 
 for i in matriz:
     print(i)
+print('\n', dicionario_de_numeros)
