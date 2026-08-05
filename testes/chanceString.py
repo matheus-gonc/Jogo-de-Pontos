4c6d516e8d811e36adc54940e758f334c4dc3f0a
Matheus Gonçalves de Souza <matheusgsouza35@gmail.com>
2026-08-05 17:05:02 -0300

    Update chanceString.py

--- testes/chanceString.py
+++ testes/chanceString.py
@@ -2,10 +2,10 @@ import random
 from collections import Counter
 
 # 1. Definir a lista de strings
-participantes = ["Python", "Java", "C++", "JavaScript"]
+participantes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
 # 2. Definir a quantidade de simulações
-total_simulacoes = 100
+total_simulacoes = 10
 
 # 3. Executar o sorteio aleatório 100 vezes
 resultados = [random.choice(participantes) for _ in range(total_simulacoes)]
@@ -19,8 +19,8 @@ for string in participantes:
     # Vezes sorteada dividido por 100 simulações multiplicado por 100 resulta no próprio contador
     frequencia_porcentagem = (contagem[string] / total_simulacoes) * 100
     
-    print(f"String: {string:<11} | Sorteada: {contagem[string]:>2} vezes | Chance Real: {frequencia_porcentagem:.1f}%")
+    print(f"String: {string:<11} | Sorteada: {contagem[string]:>2} vezes | Chance Real: {frequencia_porcentagem:.3f}%")
 
 # Chance teórica matemática
 chance_teorica = (1 / len(participantes)) * 100
-print(f"\n* A chance teórica exata para cada uma era de: {chance_teorica:.1f}%")
+print(f"\n* A chance teórica exata para cada uma era de: {chance_teorica:.3f}%")
